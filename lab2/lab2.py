ee421d0113dd49a1558dd0e1b6876679712aa3b0
Anastasiya <90769448+soplyashka@users.noreply.github.com>
2023-04-12 11:46:21 +0300

    add_lab2

---
+++ lab2/lab2.py
@@ -0,0 +1,23 @@
+bad_hosts = []
+with open('host1.txt') as file:
+    for line in file.readlines()[27:]:
+        if line[0] == '#':
+            continue
+        bad_hosts.append(line.split()[1])
+
+#print(bad_hosts)
+
+hosts = []
+with open('dns.log') as file:
+    for line in file.readlines():
+        if line[0] == '#':
+            continue
+        try:
+            hosts.append(line.split()[9])
+        except IndexError:
+            continue
+
+bad_count = len([host for host in hosts if host in bad_hosts])
+percentile = round(bad_count/len(hosts),3)*100
+print("Число вхождений DNS в список нежелательного трафика: {}.".format(str(bad_count)),
+"Процент нежелательного трафика: {}%.".format(str(percentile)),sep='\n')
